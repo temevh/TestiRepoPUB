293e86b49419ec92613400ad4a43be0039927634
temeh <temeh@DESKTOP-G81PITE>
2022-01-12 12:53:02 +0200

    Print lisäystä

--- TestProject/src/moikkaMaailma.java
+++ TestProject/src/moikkaMaailma.java
@@ -3,7 +3,8 @@ public class moikkaMaailma {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.print("Moikka maailma!");
+		System.out.print("Moikka maailma!\n");
+		System.out.print("Toimiiks tää? Näkyykö repossa?");
 
 	}
 
